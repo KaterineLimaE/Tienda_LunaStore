47f09bea180b2b46b9ccd22cb0450fbf00119dc5
KaterineLimaE <katerinecandylimaesperilla@gmail.com>
2023-08-09 20:26:58 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Luna Store' });
+});
+
+router.get('/catalogo', function(req, res, next) {
+  res.render('catalogo', { title: 'Luna Store' });
+});
+
+router.get('/admi', function(req, res, next) {
+  res.render('admi', { title: 'Luna Store' });
+});
+
+router.get('/insertar', function(req, res, next) {
+  res.render('insertar', { title: 'Luna Store' });
+});
+
+router.get('/mostrar', function(req, res, next) {
+  res.render('mostrar', { title: 'Luna Store' });
+});
+
+router.get('/actualizar', function(req, res, next) {
+  res.render('actualizar', { title: 'Luna Store' });
+});
+
+router.get('/informe', function(req, res, next) {
+  res.render('informe', { title: 'Luna Store' });
+});
+
+module.exports = router;
