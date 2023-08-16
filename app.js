6b957c1dd17b9e4c1e51e227d963d453d5059bc6
Shaiel7 <142132157+Shaiel7@users.noreply.github.com>
2023-08-15 21:31:48 -0500

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const  mysql = require ( 'mysql' ) ; 
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,40 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+// *********************************************************************************
+// Configura las credenciales de conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '201229',
+  database: 'BD_Luna',
+});
+
+// Conecta a la base de datos
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+
+  console.log('Conexión exitosa a la base de datos');
+});
+
+app.get('/mostrar', (req, res) => { // Cambiado de '/' a '/mostrar'
+  const tableName = 'TPrendas';
+  const sqlQuery = `SELECT * FROM ${tableName}`;
+
+  connection.query(sqlQuery, (err, results) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta:', err);
+      return res.status(500).send('Error al obtener los datos de la base de datos');
+    }
+    res.render('mostrar', { results });
+  });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +56,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
